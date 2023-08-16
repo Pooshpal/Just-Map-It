37b7d20f32f495fd3fe29c876152ad270e2a857c
Pooshpal Baheti <pooshpalbaheti@gmail.com>
2023-08-16 20:02:33 +0530

    welcome

--- Network API/TCP_client.py
+++ Network API/TCP_client.py
@@ -4,7 +4,7 @@ import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
-host = "10.20.205.221"
+host = "10.20.204.141"
 
 # define the port on which to connect
 port = 12345
@@ -12,7 +12,9 @@ port = 12345
 # connect to the server
 client_socket.connect((host, port))
 
-# send the filename to the server
+client_socket.sendall(b"Hello Server")
+data = client_socket.recv(1024)
+"""# send the filename to the server
 filename = "example.jpg"
 client_socket.send(filename.encode())
 
@@ -27,6 +29,6 @@ while True:
 # write the file contents to a file
 with open(filename, "wb") as file:
     file.write(file_data)
-
+"""
 # close the connection
 client_socket.close()
