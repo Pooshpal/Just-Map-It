1aba96219b6aa4e00595dac0d0f1d878666efa4c
Pooshpal Baheti <pooshpalbaheti@gmail.com>
2023-10-12 11:05:18 +0530

    hi

--- src/Backend/myproject/myproject/settings.py
+++ src/Backend/myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1j608%hlq$m6+!^hm21+%alq-+nr#f=#ckhmwwoinm+w$+dgg=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
