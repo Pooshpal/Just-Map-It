67a39f8896762decbb924e2acac64fd1ffd5265c
Pooshpal Baheti <pooshpalbaheti@gmail.com>
2023-10-17 12:54:21 +0530

    yay

--- src/Backend/myproject/backend.py
+++ src/Backend/myproject/backend.py
@@ -22,7 +22,7 @@ def getImage(path:list):
     img = cv2.imread('./assets/floor_blueprint.png')
     for i in range(len(path)-1):
         cv2.line(img,(int(path[i][0]),int(path[i][1])),(int(path[i+1][0]),int(path[i+1][1])),(0, 0, 255), 2)
-    cv2.imwrite("./assets/final_path.png",img)
+    #cv2.imwrite("./assets/final_path.png",img)
      # Encode the image as base64
     _, encoded_image = cv2.imencode('.png', img)
     base64_image = base64.b64encode(encoded_image.tobytes()).decode()
