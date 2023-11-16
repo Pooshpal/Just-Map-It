c2b8508b7150687c3e903ea34c96838cbfc87a0c
Paladugula Lakshmi Snigdha <76674154+snigdha1012@users.noreply.github.com>
2023-11-16 11:18:18 +0530

    Delete src directory

--- src/Backend/myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import get_map, get_metadata,  get_directions,get_all_records
-
-urlpatterns = [
-    path('getMap/', get_map, name='get_map'),
-    path('getMetadata/', get_metadata, name='get_metadata'),
-    path('getDirections/', get_directions, name='get_directions'),
-    path('all/', get_all_records, name='get_all_records'),
-]
